5440135f8f803b89ae33b325611a36b2b8d72721
solomonbaez <solomon.james.baez@gmail.com>
2023-08-18 17:17:33 -0700

    Expanded Value class unit tests

--- micrograd/tests.py
+++ micrograd/tests.py
@@ -1,13 +1,18 @@
 from engine import Value
 
+########## Value methods ##########
 a = Value(2)
 b = Value(1)
 
 print("a:", a)
 print("b:", b)
 
-c = Value(a + b)
+c = a + b
 print("c:", c)
+print("c - children:", c._prv)
+print("c - operation:", c._op)
 
-d = c - b
+d = c * b
 print("d:", d)
+print("d - children:", d._prv)
+print("d - operation:", d._op)
