ab9551324037a3efb39225b2c650b3a34308d19b
solomonbaez <solomon.james.baez@gmail.com>
2023-08-18 17:12:41 -0700

    Added child set to Value class and restructured method returns

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,27 +1,29 @@
 class Value:
     # wrapper
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _operation=""):
         self.data = data
+        self._prv = set(_children)
+        self._op = _operation
 
     def __repr__(self) -> str:
         return f"Value(data={self.data})"
 
     def __add__(self, operand) -> int:
-        result = self.data + operand.data
+        result = Value((self.data + operand.data), (self, operand), "+")
 
         return result
 
     def __sub__(self, operand) -> int:
-        result = self.data - operand.data
+        result = Value((self.data - operand.data), (self, operand), "-")
 
         return result
 
     def __mul__(self, operand) -> int:
-        result = self.data * operand.data
+        result = Value((self.data * operand.data), (self, operand), "*")
 
         return result
 
     def __div__(self, operand) -> int:
-        result = self.data / operand.data
+        result = Value((self.data / operand.data), (self, operand), "/")
 
         return result
